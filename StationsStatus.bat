596efe744808daaa5165d2d1d0a69b4ace41f107
ejavier8205 <32713785+ejavier8205@users.noreply.github.com>
2018-11-10 23:09:09 -0500

    changed look

--- StationsStatus.bat
+++ StationsStatus.bat
@@ -7,16 +7,19 @@ type top.html>StationsStatus.html
 set "CustomerID="
 
 
-sort status.txt>status_sorted.txt
 
-FOR /f "tokens=1,2,3,4 delims=," %%A in (status_Sorted.txt) do (
+FOR /f "tokens=1,2,3,4 delims=," %%A in (status.txt) do (
     set "CustomerIP=%%B"
     if /i "!CustomerIP!" EQU "192.168.8.1" Set "CustomerID=PWCS"
     if /i "!CustomerIP!" EQU "192.168.1.1" Set "CustomerID=GHOST"
     if /i "!CustomerIP!" EQU "192.168.1.5" Set "CustomerID=AACPS"
 
-
-    echo ^<div id="!CustomerID!" class="status"^>%%A  !CustomerID! %%B ^<a class="date"^>%%C^</a^>^</div^>>>StationsStatus.html
+    echo ^<tr class="tableRows"^>>>StationsStatus.html
+    echo ^<td id="!CustomerID!" class="customer"^>!CustomerID!^</td^>>>StationsStatus.html 
+    echo ^<td id="StationID" class="station"^>%%A^</td^>>>StationsStatus.html
+    echo ^<td id="CustomerIP" class="IP"^>%%B^</td^>>>StationsStatus.html
+    echo ^<td id="DateID" class="Date"^>%%C^</td^>>>StationsStatus.html 
+    echo ^</tr^>>>StationsStatus.html
 
     )
 type bottom.html>>StationsStatus.html
