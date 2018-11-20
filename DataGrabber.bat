ad1a602b6816a7614ba1ee856986e81ad5289cf4
Edward Paz <ejavier8205@gmail.com>
2018-11-20 16:22:41 -0500

    nada

--- DataGrabber.bat
+++ DataGrabber.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-
+set HomeDirectory=%~dp0%
 :checkPrivileges
 NET FILE 1>NUL 2>NUL
 IF '%errorlevel%' == '0' ( GOTO :gotPrivileges ) else ( GOTO :getPrivileges )
@@ -22,7 +22,7 @@ exit \B
 
 :gotPrivileges
 
-@cd /d "%~dp0"
+cd /d %HomeDirectory%
 
 :start
 title DataGrabber
@@ -47,11 +47,10 @@ timeout /t 5 >nul 2>nul
 
 Set "NoConnection=Disconnected"
 set "NodeID=Node1-Status.txt"
+
 Set StationID=0
 set TotalStations=0
-type nul>!NodeID!
-
-
+type nul>"%HomeDirectory%!NodeID!"
 
 ::GET GATEWAY
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -67,21 +66,21 @@ netsh interface ip show addresses name="!StationName! !StationID!" | find /i "De
 
 
 :SaveGateway
-netsh interface ip show addresses name="!StationName! !StationID!" | find /i "Default Gateway">>temp.txt
-for /f "tokens=2 delims=:" %%a in  (temp.txt) do (
+netsh interface ip show addresses name="!StationName! !StationID!" | find /i "Default Gateway">>"%HomeDirectory%temp.txt"
+for /f "tokens=2 delims=:" %%a in  ('TYPE "%HomeDirectory%temp.txt"') do (
     set "gateway=%%a"
     set "gateway=!gateway: =!"
 )
 set "MyTime=%time%"
 Set "MyTime=!MyTime: =!"
-echo !StationName! !StationID!,!Gateway!,%date%,!Mytime!>>!NodeID!
+echo !StationName! !StationID!,!Gateway!,%date%,!Mytime!>>"%HomeDirectory%!NodeID!"
 set /a TotalStations+=1
 goto :NextStation
 
 :NoGateway
 set "MyTime=%time%"
 Set "MyTime=!MyTime: =!"
-echo !StationName! !StationID!,!NoConnection!,%date%,!Mytime!>>!NodeID!
+echo !StationName! !StationID!,!NoConnection!,%date%,!Mytime!>>"%HomeDirectory%!NodeID!"
 set /a TotalStations+=1
 goto :NextStation
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
