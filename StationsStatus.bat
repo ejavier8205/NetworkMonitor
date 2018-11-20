9aa4d0c3bbccdae9f0558e00d557565f8eed4774
Edward Paz <ejavier8205@gmail.com>
2018-11-20 16:21:46 -0500

    Remote Directory adpapt

--- StationsStatus.bat
+++ StationsStatus.bat
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal EnableDelayedExpansion
-
+set HomeDirectory=%~dp0%
 :checkPrivileges
 NET FILE 1>NUL 2>NUL
 IF '%errorlevel%' == '0' ( GOTO :gotPrivileges ) else ( GOTO :getPrivileges )
@@ -23,18 +23,21 @@ exit \B
 
 :gotPrivileges
 
-@cd /d "%~dp0"
+cd /d %HomeDirectory%
 
 
 :start
 TITLE Stations Status
-type top.html>StationsStatus.html
-type nul>statustable.txt
+
+
+
+type "%HomeDirectory%top.html">"%HomeDirectory%StationsStatus.html"
+type nul>"%HomeDirectory%StatusTable.txt"
 set "CustomerID="
 
 
 
-FOR /f "tokens=1,2,3,4,5 delims=," %%A in (Node1-status.txt) do (
+FOR /f "tokens=1,2,3,4,5 delims=," %%A in ('TYPE "%HomeDirectory%Node1-status.txt"') do (
     set "CustomerIP=%%B"
     if /i "!CustomerIP!" EQU "192.168.2.1" Set "CustomerID=ASG LAB"
     if /i "!CustomerIP!" EQU "Disconnected" Set "CustomerID=Not Connected"
@@ -43,22 +46,22 @@ FOR /f "tokens=1,2,3,4,5 delims=," %%A in (Node1-status.txt) do (
     if /i "!CustomerIP!" EQU "192.168.0.1" Set "CustomerID=BALTIMORE CO."
     if /i "!CustomerIP!" EQU "10.255.248.1" Set "CustomerID=LOUDOUN CO."
 
-    echo ^<tr class="tableRows"^>>>StatusTable.txt
+    echo ^<tr class="tableRows"^>>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^<td id="!CustomerID!" class="customer"^>!CustomerID!^</td^>>>StatusTable.txt
+    echo ^<td id="!CustomerID!" class="customer"^>!CustomerID!^</td^>>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^<td id="StationID" class="station"^>%%A^</td^>>>StatusTable.txt
+    echo ^<td id="StationID" class="station"^>%%A^</td^>>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^<td id="CustomerIP" class="IP"^>%%B^</td^>>>StatusTable.txt
+    echo ^<td id="CustomerIP" class="IP"^>%%B^</td^>>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^<td id="DateID" class="Date"^>%%C^</td^>>>StatusTable.txt
+    echo ^<td id="DateID" class="Date"^>%%C^</td^>>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^<td id="TimeID" class="Time"^>%%D^</td^>>>StatusTable.txt
+    echo ^<td id="TimeID" class="Time"^>%%D^</td^>>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^</tr^>>>StatusTable.txt
+    echo ^</tr^>>>"%HomeDirectory%StatusTable.txt"
     )
-type statustable.txt>>StationsStatus.html
-type bottom.html>>StationsStatus.html
+type "%HomeDirectory%statustable.txt">>"%HomeDirectory%StationsStatus.html"
+type "%HomeDirectory%bottom.html">>"%HomeDirectory%StationsStatus.html"
 timeout /t 1 >nul 2>nul
 goto :start
 endlocal
