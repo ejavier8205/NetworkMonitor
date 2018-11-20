72241c24f5a40f11f0c23a452958364996854874
Edward Paz <ejavier8205@gmail.com>
2018-11-20 12:36:29 -0500

    cosmetic

--- StationsStatus.bat
+++ StationsStatus.bat
@@ -1,6 +1,30 @@
 @echo off
+TITLE stations status
+setlocal EnableDelayedExpansion
 
-setlocal enabledelayedexpansion
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+IF '%errorlevel%' == '0' ( GOTO :gotPrivileges ) else ( GOTO :getPrivileges )
+
+:getPrivileges
+IF '%1'=='ELEV' (shift & GOTO :gotPrivileges)
+ECHO.
+ECHO *****************
+ECHO Invoking UAC For Privilege Escalation
+ECHO *****************
+
+setlocal DisableDelayedExpansion
+SET "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO SET UAC = CreateObject^("Shell.Application"^) > "%temp%\UAC.vbs"
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\UAC.vbs"
+"%temp%\UAC.vbs"
+exit \B
+
+:gotPrivileges
+
+@cd /d "%~dp0"
+
 
 :start
 type top.html>StationsStatus.html
@@ -11,9 +35,12 @@ set "CustomerID="
 
 FOR /f "tokens=1,2,3,4,5 delims=," %%A in (Node1-status.txt) do (
     set "CustomerIP=%%B"
-    if /i "!CustomerIP!" EQU "192.168.2.1" Set "CustomerID=LAB"
+    if /i "!CustomerIP!" EQU "192.168.2.1" Set "CustomerID=ASG LAB"
     if /i "!CustomerIP!" EQU "Disconnected" Set "CustomerID=Not Connected"
-    if /i "!CustomerIP!" EQU "192.168.1.5" Set "CustomerID=AACPS"
+    if /i "!CustomerIP!" EQU "192.168.8.1" Set "CustomerID=PRINCE WILLIAM CO."
+    if /i "!CustomerIP!" EQU "192.168.24.251" Set "CustomerID=GHOST"
+    if /i "!CustomerIP!" EQU "192.168.0.1" Set "CustomerID=BALTIMORE CO."
+    if /i "!CustomerIP!" EQU "10.255.248.1" Set "CustomerID=LOUDOUN CO."
 
     echo ^<tr class="tableRows"^>>>StatusTable.txt
 
