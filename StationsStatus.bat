5320753cee9685e9f9729a70be621c769de428cd
Edward Paz <ejavier8205@gmail.com>
2018-11-21 15:37:42 -0500

    colors

--- StationsStatus.bat
+++ StationsStatus.bat
@@ -40,16 +40,16 @@ set "CustomerID="
 
 FOR /f "tokens=1,2,3,4,5 delims=," %%A in ('TYPE "%HomeDirectory%Node1-status.txt"') do (
     set "CustomerIP=%%B"
-    if /i "!CustomerIP!" EQU "192.168.2.1" Set "CustomerID=ASG LAB"
-    if /i "!CustomerIP!" EQU "Disconnected" Set "CustomerID=Not Connected"
-    if /i "!CustomerIP!" EQU "192.168.8.1" Set "CustomerID=PRINCE WILLIAM CO."
-    if /i "!CustomerIP!" EQU "192.168.24.251" Set "CustomerID=GHOST"
-    if /i "!CustomerIP!" EQU "192.168.0.1" Set "CustomerID=BALTIMORE CO."
-    if /i "!CustomerIP!" EQU "10.255.248.1" Set "CustomerID=LOUDOUN CO."
-    if /i "!CustomerIP!" EQU "192.168.16.250" Set "CustomerID=ANNE ARUNDEL CO."		
+    if /i "!CustomerIP!" EQU "192.168.2.1" Set "CustomerID=ASG LAB" && Set "CustomerAbbr=ASGLAB"
+    if /i "!CustomerIP!" EQU "Disconnected" Set "CustomerID=Not Connected" && Set "CustomerAbbr=DISCONNECTED"
+    if /i "!CustomerIP!" EQU "192.168.8.1" Set "CustomerID=PRINCE WILLIAM CO." && Set "CustomerAbbr=PWCS"
+    if /i "!CustomerIP!" EQU "192.168.24.251" Set "CustomerID=GHOST" && Set "CustomerAbbr=GHOST"
+    if /i "!CustomerIP!" EQU "192.168.0.1" Set "CustomerID=BALTIMORE CO." && Set "CustomerAbbr=BOE-BCPS"
+    if /i "!CustomerIP!" EQU "10.255.248.1" Set "CustomerID=LOUDOUN CO." && Set "CustomerAbbr=LCPS"
+    if /i "!CustomerIP!" EQU "192.168.16.250" Set "CustomerID=ANNE ARUNDEL CO." && Set "CustomerAbbr=AACPS"		
     echo ^<tr class="tableRows"^>>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^<td id="!CustomerID!" class="customer"^>!CustomerID!^</td^>>>"%HomeDirectory%StatusTable.txt"
+    echo ^<td id="!CustomerAbbr!" class="customer"^>!CustomerID!^</td^>>>"%HomeDirectory%StatusTable.txt"
 
     echo ^<td id="StationID" class="station"^>%%A^</td^>>>"%HomeDirectory%StatusTable.txt"
 
