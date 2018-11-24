1969c1576d90f438ac57e3e011399ca73db13f72
Edward Paz <ejavier8205@gmail.com>
2018-11-24 17:41:36 -0500

    search bar relocation

--- StationsStatus.bat
+++ StationsStatus.bat
@@ -68,7 +68,7 @@ set "CurrentTime=!CurrentTime: =!"
 Set "CurrentTime=!CurrentTime:~6,-3!"
 echo !CurrentTime!
 
-if '!CurrentTime!' EQU '0' (
+if '!CurrentTime!' EQU '00' (
 type "%HomeDirectory%top.html">"%HomeDirectory%StationsStatus.html"
 type "%HomeDirectory%statustable.txt">>"%HomeDirectory%StationsStatus.html"
 type "%HomeDirectory%bottom.html">>"%HomeDirectory%StationsStatus.html"
