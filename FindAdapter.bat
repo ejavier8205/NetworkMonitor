231a5d4fc2b6ab5a293fc956fc06e04921c179ac
Edward Paz <ejavier8205@gmail.com>
2018-11-28 00:35:09 -0500

    MAC Filtering

--- FindAdapter.bat
+++ FindAdapter.bat
@@ -26,23 +26,54 @@ cd /d "%HomeDirectory%"
 
 :start
 
-getmac /v /fo list>"%HomeDirectory%Nodes\%Computername% Connected Adapters.txt"
+
+getmac /v /fo csv /nh>"%HomeDirectory%Nodes\%Computername% Connected Adapters.txt"
+
+type nul>"%HomeDirectory%Nodes\%computername% Active Connections.txt"
 
 
-
-for /f "tokens=1,2,3,4 delims=," %%a in  ('TYPE "%HomeDirectory%Nodes\%computername% Adapters ID.txt"') do (
+for /f "tokens=1,2,3 delims=," %%a in  ('TYPE "%HomeDirectory%Nodes\Node1 Adapters ID.txt"') do (
+    Set MyAdaptersCount=0
+    set NotFoundCount=0
     Set "AdapterName=%%a"
     Set "AdapterDesc=%%b"
     Set "AdapterMAC=%%c"
-    Set "AdapterTCPID=%%d"
 
     Set AdapterName=!AdapterName:"=!
+    Set AdapterDesc=!AdapterDesc:"=!
+    Set AdapterMAC=!AdapterMAC:"=!
 
-    echo --!AdapterName!--      !AdapterDesc!       !AdapterMAC!        !AdapterTCPID!
+    for /f "tokens=1,2,3 delims=," %%a in  ('TYPE "%HomeDirectory%Nodes\%computername% Connected Adapters.txt"') do (
+        Set /a MyAdaptersCount+=1
+    )
+
+ 
+
+    for /f "tokens=1,2,3 delims=," %%a in  ('TYPE "%HomeDirectory%Nodes\%computername% Connected Adapters.txt"') do (
+        Set "MyAdapterName=%%a"
+        Set "MyAdapterDesc=%%b"
+        Set "MyAdapterMAC=%%c"
+
+        Set MyAdapterName=!MyAdapterName:"=!
+        Set MyAdapterDesc=!MyAdapterDesc:"=!
+        Set MyAdapterMAC=!MyAdapterMAC:"=!
+
+        if /i !MyAdapterMAC! == !AdapterMAC! (
+            echo !AdapterName!,!MyAdapterName!,!MyAdapterMAC!>>"%HomeDirectory%Nodes\%computername% Active Connections.txt"
+        ) else (
+            Set /a NotFoundCount+=1
+        )     
+
+    )
+
+        if /i !MyAdaptersCount! == !NotFoundCount! (
+            echo !AdapterName!,Not Available,Not Available>>"%HomeDirectory%Nodes\%computername% Active Connections.txt"
+        )
 
 )
 
-for /f "tokens=2 delims=:" %%a in  ('TYPE "%HomeDirectory%Nodes\%computername% Connected Adapters.txt"') do (
-    echo --%%a--)
+
+
+
     pause
 endlocal
