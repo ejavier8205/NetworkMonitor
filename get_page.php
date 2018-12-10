a33924db31d2edd05ba5337c4bf53dc0029cd13c
ejavier8205 <32713785+ejavier8205@users.noreply.github.com>
2018-12-09 20:46:10 -0500

    php and ajac tests

---
+++ get_page.php
@@ -0,0 +1,2 @@
+<?phprequire_once("dbcontroller.php");$db_handle = new DBController();$pages = $db_handle->runQuery("SELECT * FROM pages WHERE id = ".$_REQUEST['id']);if(!empty($pages)) {
+?><h3><?php echo $pages[0]['title'];?></h3><div><?php echo $pages[0]['content'];?></p><?php } ?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?phprequire_once("dbcontroller.php");$db_handle = new DBController();$pages = $db_handle->runQuery("SELECT * FROM pages");?><html>
+<head>
+<title>Load Dynamic Content using jQuery AJAX</title><script src="http://code.jquery.com/jquery-2.1.1.js"></script><style type="text/css" media="screen">	body{width:610;}	#menu{background: #D8F9D3;height: 40px;border-top: #F0F0F0 2px solid;}	#menu input[type="button"]{margin-left: 2px;padding: 0px 15px;height: 40px;border: 0px;background: #F0F0F0;}
+	#output{min-height:300px;border:#F0F0F0 1px solid;padding:15px;}
+</style><script type="text/javascript">function getPage(id) {	$('#output').html('<img src="LoaderIcon.gif" />');	jQuery.ajax({		url: "get_page.php",		data:'id='+id,		type: "POST",		success:function(data){$('#output').html(data);}	});}getPage(1);
+</script>
+</head>
+<body><?php if(!empty($pages)) {?>
+<div id="menu"><?php foreach($pages as $page) { ?><input type="button" value="<?php echo $page["title"]; ?>" onClick="getPage(<?php echo $page["id"]; ?>);" /><?php }?>	
+</div><?php } ?>	<div id="output"></div>
+</body>
+</html>
