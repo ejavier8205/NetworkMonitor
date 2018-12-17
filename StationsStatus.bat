0d85283c1dc28190d356dcffa1b045f02eb44ebe
ejavier8205 <32713785+ejavier8205@users.noreply.github.com>
2018-12-16 23:48:11 -0500

    change bat

--- StationsStatus.bat
+++ StationsStatus.bat
@@ -33,6 +33,7 @@ TITLE Stations Status
 
 type nul>"%HomeDirectory%StatusTable.txt"
 
+
 set "CustomerID="
 
 FOR /f  %%A in ('dir "%HomeDirectory%Nodes\AllNodesCompilation" /b /A:-D') do (
@@ -40,8 +41,10 @@ FOR /f  %%A in ('dir "%HomeDirectory%Nodes\AllNodesCompilation" /b /A:-D') do (
 
 
 FOR /f "tokens=1,2,3,4,5 delims=," %%A in ('TYPE "%HomeDirectory%Nodes\AllNodesCompilation\!NodeDataFile!"') do (
+    Set "StationName=%%A"
     set "CustomerIP=%%B"
-    Set "RowID=Row"
+    Set "CurrentDate=%%C"
+    Set "CurrentTime=%%D"
     if /i "!CustomerIP!" EQU "192.168.2.1" Set "CustomerID=ASG LAB" && Set "CustomerAbbr=ASGLAB"
     if /i "!CustomerIP!" EQU "Disconnected" Set "CustomerID=Not Connected" && Set "CustomerAbbr=DISCONNECTED" && Set "RowID=DisconnectedRow"
     if /i "!CustomerIP!" EQU "192.168.8.1" Set "CustomerID=PRINCE WILLIAM CO." && Set "CustomerAbbr=PWCS"
@@ -49,19 +52,9 @@ FOR /f "tokens=1,2,3,4,5 delims=," %%A in ('TYPE "%HomeDirectory%Nodes\AllNodesC
     if /i "!CustomerIP!" EQU "192.168.0.1" Set "CustomerID=BALTIMORE CO." && Set "CustomerAbbr=BOE-BCPS"
     if /i "!CustomerIP!" EQU "10.255.248.1" Set "CustomerID=LOUDOUN CO." && Set "CustomerAbbr=LCPS"
     if /i "!CustomerIP!" EQU "192.168.16.250" Set "CustomerID=ANNE ARUNDEL CO." && Set "CustomerAbbr=AACPS"		
-    echo ^<tr Id="!RowID!" class="tableRows"^>>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^<td id="!CustomerAbbr!" class="customer"^>!CustomerID!^</td^>>>"%HomeDirectory%StatusTable.txt"
+    echo !CustomerID!,!StationName!,!CustomerIP!,!CurrentDate!,!CurrentTime!>>"%HomeDirectory%StatusTable.txt"
 
-    echo ^<td id="StationID" class="station"^>%%A^</td^>>>"%HomeDirectory%StatusTable.txt"
-
-    echo ^<td id="CustomerIP" class="IP"^>%%B^</td^>>>"%HomeDirectory%StatusTable.txt"
-
-    echo ^<td id="DateID" class="Date"^>%%C^</td^>>>"%HomeDirectory%StatusTable.txt"
-
-    echo ^<td id="TimeID" class="Time"^>%%D^</td^>>>"%HomeDirectory%StatusTable.txt"
-
-    echo ^</tr^>>>"%HomeDirectory%StatusTable.txt"
     )
 
 )
@@ -79,9 +72,8 @@ FOR /f "tokens=1,2,3,4,5 delims=," %%A in ('TYPE "%HomeDirectory%Nodes\AllNodesC
 
 
 if '!CurrentTime!' EQU '00' (
-    type "%HomeDirectory%top.html">"%HomeDirectory%StationsStatus.html"
-    type "%HomeDirectory%statustable.txt">>"%HomeDirectory%StationsStatus.html"
-    type "%HomeDirectory%bottom.html">>"%HomeDirectory%StationsStatus.html"
+    type nul>"%HomeDirectory%Status.txt"
+    type "%HomeDirectory%statustable.txt">>"%HomeDirectory%Status.txt"
     echo data sent
 goto :start 
 ) else (
